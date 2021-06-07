924f60a5ef0b61eb63b39fc0f19ed7c82f665154
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-06-07 14:21:14 -0500

    Extend the training data and add parameter to the get_emoji func

--- emojex/__init__.py
+++ emojex/__init__.py
@@ -1,22 +1,27 @@
 import openai
 
+
+def set_api_key(key):
+    openai.api_key = key
+
+def get_emoji(text, max_tokens=30):
+    prompt = "Back to Future: 👨👴🚗🕒\nBatman: 🤵🦇\nTransformers: 🚗🤖\nWonder Woman: 👸🏻👸🏼👸🏽👸🏾👸🏿\nWinnie the Pooh: 🐻🐼🐻\nThe Godfather: 👨👩👧🕵🏻‍♂️👲💥\nGame of Thrones: 🏹🗡🗡🏹\n" + text + ":"
+    engine = "davinci"
+
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=max_tokens,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\n"]
+    )
+
+    return response["choices"][0]["text"]
+
 if __name__ == "__main__":
     def set_api_key(key):
         openai.api_key = key
 
-    def get_emoji():
-        prompt = "Back to Future: 👨👴🚗🕒\nBatman: 🤵🦇\nTransformers: 🚗🤖\nWonder Woman: 👸🏻👸🏼👸🏽👸🏾👸🏿\nWinnie the Pooh: 🐻🐼🐻\nThe Godfather: 👨👩👧🕵🏻‍♂️👲💥\nGame of Thrones: 🏹🗡🗡🏹\n" + text + ":"
-        engine = "davinci"
-
-        response = openai.Completion.create(
-            engine=engine,
-            prompt=prompt,
-            temperature=0.6,
-            max_tokens=max_tokens,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=["\n"]
-        )
-
-        return response["choices"][0]["text"]
